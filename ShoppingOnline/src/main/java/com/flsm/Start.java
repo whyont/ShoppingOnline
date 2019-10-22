e58d36521a50f450e8a575ec2c037354da12fcc5
15404 <15404@DESKTOP-IF9UBUQ>
2019-10-22 11:06:13 +0800

    1.4

--- ShoppingOnline/src/main/java/com/flsm/Start.java
+++ ShoppingOnline/src/main/java/com/flsm/Start.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Start {
 	public static void main(String[] args) {
 		SpringApplication.run(Start.class, args);
-		System.out.println(1234456);
 	}
 
 }
